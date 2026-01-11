d5a41ce3237840ff36d402696d1993147ec67914
Garv Mittal <garv.mittal31@gmail.com>
2026-01-11 13:57:08 +0530

    added relative path

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ website_entry.grid(row= 1,column= 1,columnspan=2)
 website_entry.focus()
 email_entry = Entry(width= 35)
 email_entry.grid(row= 2,column= 1,columnspan=2)
-email_entry.insert(0,"kusha.mittal21@gmail.com")
+email_entry.insert(0,"sample@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(row= 3,column= 1,columnspan=2)
 
